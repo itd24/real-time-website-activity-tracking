0968d35794cac3d0f816c0c4b4d4c51188d9da57
Ziga Petek <pezi@auris-consult.at>
2024-11-30 09:55:21 +0100

    Connected to Schema Registry

--- shop/src/main/java/com/example/shop/service/configuration/KafkaProducerConfig.java
+++ shop/src/main/java/com/example/shop/service/configuration/KafkaProducerConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import com.example.shop.model.TrackingEvent;
+import io.confluent.kafka.serializers.KafkaJsonSerializer;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,16 +18,17 @@ import java.util.Map;
 public class KafkaProducerConfig {
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, TrackingEvent> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaJsonSerializer.class);
+        configProps.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://apicurio-registry:8080/apis/ccompat/v7");
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, TrackingEvent> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
